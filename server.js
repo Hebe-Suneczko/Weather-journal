b3e2523d17ffa2672df52618511c252d4282ecb7
hebe <happyhebenet@hotmail.com>
2020-10-01 01:21:22 +0200

    get API
    Add local server
    get API
    update danamic UI

--- server.js
+++ server.js
@@ -2,18 +2,66 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* Dependencies */
+const bodyParser = require('body-parser')
+    /* Middleware*/
+    //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+const { request } = require('http');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+
+/* Spin up the server*/
+const server = app.listen(port, listening);
+
+function listening() {
+    // console.log(server);
+    console.log(`running on http://localhost:${port}`);
+};
+
+
+
+
+
+// GET route
+app.get('/api/entry', sendData);
+
+function sendData(request, response) {
+    response.send(projectData);
+};
+
+// POST route
+
+app.post('/api/entry', callBack);
+
+function callBack(req, res) {
+    const postData = req.body;
+
+    projectData.temp = postData.temp;
+    projectData.country = postData.country;
+    projectData.place = postData.place;
+    projectData.userResponse = postData.userResponse;
+    projectData.sunrise = postData.sunrise;
+    projectData.sunset = postData.sunset;
+    projectData.windspeed = postData.windspeed;
+    projectData.date = postData.date;
+    projectData.humidity = postData.humidity;
+    projectData.cloudiness = postData.cloudiness;
+
+
+    res.send(projectData);
+};
--- website/app.js
+++ website/app.js
@@ -1,5 +1,110 @@
 /* Global Variables */
+const API_KEY = 'd09f48689a0aa2d5636748f8b00f237e';
 
-// Create a new date instance dynamically with JS
+//Get Today Date
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let today = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+//URL
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
+
+// Async GET,Function to GET Web API Data
+const getWeatherData = async(zipCode) => {
+    const zipUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}`;
+    const request = await fetch(zipUrl);
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+        console.log(allData);
+        return allData;
+    } catch (error) {
+        console.log("error", error);
+        return null;
+        // appropriately handle the error
+    }
+};
+
+
+
+/* Event Listener for generate button */
+document.querySelector("#generate").addEventListener("click", () => {
+    const zipCode = document.querySelector('#zip').value.trim();
+    const feelings = document.querySelector('#feelings').value;
+    getWeatherData(zipCode)
+        .then(weatherData => {
+            console.log(weatherData);
+            postData("/api/entry", {
+                place: weatherData.name,
+                date: today,
+                country: weatherData.sys.country,
+                cloudiness: weatherData.weather[0].description,
+                //nearest integer
+                humidity: `${Math.round(weatherData.main.humidity)} %`,
+                windspeed: `${weatherData.wind.speed} mi/h`,
+                temp: `${Math.round(weatherData.main.temp)} °F`,
+                sunrise: UTC(weatherData.sys.sunrise),
+                sunset: UTC(weatherData.sys.sunset),
+                userResponse: feelings
+            }).then(
+                retrieveData("/api/entry")
+            )
+        })
+
+});
+
+const UTC = (unixTime) => {
+    const time = new Date(unixTime * 1000);
+    return `${time.toUTCString().slice(17, 25)} GMT`;
+}
+
+// Async POST
+const postData = async(url = '', data = { temp, date, userResponse }) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+
+// Function to GET Project Data
+const retrieveData = async(url = '') => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+
+        /* Update UI */
+        updateUI(allData);
+
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+
+};
+
+/* Update danamic UI */
+const updateUI = (weatherData) => {
+    const location = `${weatherData.place}, ${weatherData.country}`
+    document.querySelector(".location").innerHTML = location;
+    document.querySelector("#date").innerHTML = weatherData.date;
+    document.querySelector("#temp").innerHTML = weatherData.temp;
+    document.querySelector(".humidity").innerHTML = weatherData.humidity;
+    document.querySelector(".cloudiness").innerHTML = weatherData.cloudiness;
+    document.querySelector(".sunrise").innerHTML = weatherData.sunrise;
+    document.querySelector(".sunset").innerHTML = weatherData.sunset;
+    document.querySelector(".windspeed").innerHTML = weatherData.windspeed;
+    document.querySelector(".user_respones").innerHTML = weatherData.userResponse;
+}
